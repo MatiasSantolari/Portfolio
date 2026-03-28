a24c508fe5d866428d1a47e7428ae940b47a115e
Matias <santolarimatias@gmail.com>
2026-03-28 20:53:33 -0300

    chore: modificación next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,12 @@
 import type { NextConfig } from "next";
 
-const repoName = "portfolio";
+const isProd = process.env.NODE_ENV === 'production';
+const internalBasePath = process.env.NEXT_PUBLIC_BASE_PATH || "";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  basePath: `/${repoName}`,
-  assetPrefix: `/${repoName}/`,
+  output: 'export',
+  basePath: internalBasePath,
+  assetPrefix: internalBasePath, 
   images: {
     unoptimized: true,
   },
